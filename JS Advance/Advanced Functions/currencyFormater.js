019b22cf8f6ab8df879c0b4a8962170b01ce4305
Georgi Raynov <jornalo891@gmail.com>
2022-05-12 22:09:11 +0300

    Add files via upload

---
+++ JS Advance/Advanced Functions/currencyFormater.js
@@ -0,0 +1,15 @@
+function createFormatter(separator, symbol, symbolFirst, value){
+    
+}
+
+
+
+
+
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
