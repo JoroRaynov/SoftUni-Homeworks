2ee97c88e5ff8bd1779664cc64d9499884691a82
Georgi Raynov <jornalo891@gmail.com>
2022-05-19 10:23:36 +0300

    Add files via upload

---
+++ JS Fundamentals/Functions - LAB/05. RepeatString.js
@@ -0,0 +1,11 @@
+function repeatString(str, repeats){
+
+        let result = '';
+
+    for (let i = 0; i < repeats; i++) {
+        result += str
+        
+    }
+return result
+}
+console.log(repeatString('abc', 3));
