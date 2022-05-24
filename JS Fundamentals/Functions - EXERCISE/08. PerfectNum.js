47559e4268723a896020f1a2e5f5634021aac8f8
Georgi Raynov <jornalo891@gmail.com>
2022-05-24 12:12:59 +0300

    Add files via upload

---
+++ JS Fundamentals/Functions - EXERCISE/08. PerfectNum.js
@@ -0,0 +1,11 @@
+function perfectN(n){
+
+if(n == 6 || n == 28 || n == 496 || n==  8128 || n ==33550336 || n == 8589869056 || n == 137438691328){
+    console.log('We have a perfect number!');
+
+} else {
+    console.log("It's not so perfect.");
+}
+let num = 5843008139952128
+}
+perfectN(1236498)
