a8d909fb8c73d3259f203aea7fcd8fcfa6eb3b64
Georgi Raynov <jornalo891@gmail.com>
2022-05-24 12:13:28 +0300

    Add files via upload

---
+++ JS Fundamentals/Functions - EXERCISE/09. PerfectNumber.js
@@ -0,0 +1,19 @@
+function perfNum (number){
+
+    let sum = 1;
+
+    for (let i = 2; i < number; i++) {
+       
+        if(number % i === 0){
+
+            sum += i;
+        }
+        
+    }
+if(sum === number){
+    console.log('We have a perfect number!');
+} else {
+    console.log("It's not so perfect.");
+}
+}
+perfNum(28)
