965e1692461b7349505fad0d8b128b6b62353943
Georgi Raynov <jornalo891@gmail.com>
2022-05-25 10:32:22 +0300

    Add files via upload

---
+++ JS Fundamentals/Arrays - LAB/03. DayOfWeek.js
@@ -0,0 +1,13 @@
+function dayOfWeek(n){
+
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+    if(n >= 1 && n <= 7){
+        console.log(days[n-1]);
+    } else {
+        console.log('Invalid day!');
+    }
+
+}
+dayOfWeek(7)
