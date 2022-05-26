81b6a295899febf7917cae6ecc0ba22832add4b3
Georgi Raynov <jornalo891@gmail.com>
2022-05-26 12:00:59 +0300

    Add files via upload

---
+++ JS Fundamentals/Arrays - LAB/09. SumFirsAndLasArrayElements.js
@@ -0,0 +1,9 @@
+function sumFirsAndLasArrayElements(arr){
+
+    let firstSum = arr[0];
+    let secondSum = arr[arr.length - 1];
+    let result = firstSum + secondSum;
+    console.log(result);
+
+}
+sumFirsAndLasArrayElements(AbortController[11, 58, 69])
