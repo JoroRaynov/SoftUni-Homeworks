95e1ae1a6e9b933ca960351c02e504aef8dab085
Georgi Raynov <jornalo891@gmail.com>
2022-05-28 10:21:24 +0300

    Add files via upload

---
+++ JS Fundamentals/Arrays Exercise/10. MergeArray.js
@@ -0,0 +1,21 @@
+function mergeArrays (firstArray, secondArray){
+ 
+    let newArray = [];
+    
+
+
+for (let i = 0; i < firstArray.length; i++) {
+    if(i % 2 == 0){
+        newArray.push(Number(firstArray[i]) + Number(secondArray[i]));
+    } else {
+        newArray.push(firstArray[i] +  secondArray[i])
+    }
+    
+}
+
+console.log(newArray.join(' - '));
+
+    
+}
+mergeArrays(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11'])
