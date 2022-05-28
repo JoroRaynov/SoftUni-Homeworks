175de284b546922095a8a029a8f808c99e956ba2
Georgi Raynov <jornalo891@gmail.com>
2022-05-28 10:25:34 +0300

    Add files via upload

---
+++ JS Fundamentals/Object And Classes - LAB/02. City.js
@@ -0,0 +1,12 @@
+function person(firstName, lastName, age) {
+
+    let persons = {
+        firstName,
+        lastName,
+        age
+    };
+return persons;
+}
+let obj1 =
+
+console.log(person('Peter', 'Pan', '20'));
