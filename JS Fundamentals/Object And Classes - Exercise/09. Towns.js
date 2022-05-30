53bfde38673cb1ee59891e8c7da51cf45dd7f585
Georgi Raynov <jornalo891@gmail.com>
2022-05-30 23:45:19 +0300

    Add files via upload

---
+++ JS Fundamentals/Object And Classes - Exercise/09. Towns.js
@@ -0,0 +1,21 @@
+function listOfTowns(textTable) {
+
+    for (let i = 0; i < textTable.length; i++) {
+
+        let tokens = textTable[i].split(' | ')
+        let town = tokens[0];
+        let latitude = Number(tokens[1]).toFixed(2);
+        let longitude = Number(tokens[2]).toFixed(2);
+
+        let obj = {
+            town,
+            latitude,
+            longitude
+        }
+        console.log(obj);
+    }
+}
+
+listOfTowns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625'
+])
