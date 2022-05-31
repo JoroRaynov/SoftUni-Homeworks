2596d29cd136401cedb3adba5db8944f8e5f4000
Georgi Raynov <jornalo891@gmail.com>
2022-05-31 17:14:45 +0300

    Add files via upload

---
+++ JS Fundamentals/Arrays Exercise/Arrays More Exercise/02. MagicMatrices.js
@@ -0,0 +1,24 @@
+function magicMatrices(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        let sumROne = matrix[i].reduce((a, b) => a + b, 0);
+        let sumRTwo = matrix[i + 1].reduce((a, b) => a + b, 0);
+        let sumCOne = 0;
+        let sumCTwo = 0;
+
+        for (let j = 0; j < matrix.length; j++) {
+            sumCOne += matrix[i][j];
+            sumCTwo += matrix[i + 1][j];
+        }
+
+        if (sumROne !== sumRTwo || sumCOne !== sumCTwo) {
+            return false;
+        }
+    }
+
+    return true;
+}
+magicMatrices([[11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]]
+   
+   )
