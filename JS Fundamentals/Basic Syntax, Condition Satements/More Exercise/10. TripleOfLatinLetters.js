ff9ceae4377630a7a35668a9a616cd844deb776e
Georgi Raynov <jornalo891@gmail.com>
2022-06-02 21:59:57 +0300

    Add files via upload

---
+++ JS Fundamentals/Basic Syntax, Condition Satements/More Exercise/10. TripleOfLatinLetters.js
@@ -0,0 +1,15 @@
+function tripleOfLatinLetters(num){
+
+    for(let i = 0; i < num; i++){
+        let firstLetter = String.fromCharCode(97 + i)
+        for(let j = 0; j < num; j++){
+            let secondLetter = String.fromCharCode (97 + j)
+            for(let k = 0; k < num; k++){
+                let thirdLetter = String.fromCharCode(97+k);
+                console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+            }
+        }
+    }
+
+}
+tripleOfLatinLetters('3')
