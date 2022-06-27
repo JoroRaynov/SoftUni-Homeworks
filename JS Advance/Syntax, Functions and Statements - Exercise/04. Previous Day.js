9430383e120a0a2920a956a5388342722bdb2af4
Georgi Raynov <jornalo891@gmail.com>
2022-06-27 23:07:14 +0300

    Add files via upload

---
+++ JS Advance/Syntax, Functions and Statements - Exercise/04. Previous Day.js
@@ -0,0 +1,10 @@
+function prevDay(year, month, day){
+    let date = new Date(year, month-1, day);
+    date.setDate(date.getDate()-1);
+
+    console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+
+
+
+}
+prevDay(2016, 9, 30)
