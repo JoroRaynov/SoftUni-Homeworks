79712fa3cdb6d3761196be233c83f2590b187084
Georgi Raynov <jornalo891@gmail.com>
2022-06-28 20:41:45 +0300

    Create 01. Print Array with given Delimeter.js

---
+++ JS Advance/Arrays and Nested Arrays - Exercise/01. Print Array with given Delimeter.js
@@ -0,0 +1,16 @@
+function princArray(array, delimeter) {
+
+    console.log(array.join(delimeter));
+
+
+
+
+}
+princArray(['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five'
+    ],
+    '-'
+);
