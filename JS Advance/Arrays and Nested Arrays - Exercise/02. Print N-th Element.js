bc51cf2bead00457e99e524e5c64d8d2b88ab318
Georgi Raynov <jornalo891@gmail.com>
2022-06-28 20:42:00 +0300

    Add files via upload

---
+++ JS Advance/Arrays and Nested Arrays - Exercise/02. Print N-th Element.js
@@ -0,0 +1,14 @@
+function print(array, step){
+    let arr = [];
+    for(let i = 0; i < array.length; i+=step){
+    arr.push(array[i]);
+    }
+    return arr;
+}
+print(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+
+);
