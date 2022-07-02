3b94a9eb18cea1caa3ff99d5198ac38ca44c5a1c
Georgi Raynov <jornalo891@gmail.com>
2022-07-02 21:30:54 +0300

    Add files via upload

---
+++ JS Advance/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    // TODO
+    let textElemenet = document.getElementById('text');
+    let link = document.getElementById('more');
+    link.style.display = 'none'
+    textElemenet.style.display = 'inline';
+}
