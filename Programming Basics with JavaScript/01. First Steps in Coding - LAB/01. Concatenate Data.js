f38e4e6a88a53691158bbd32f3eb5f3af377f9a5
Georgi Raynov <jornalo891@gmail.com>
2022-07-22 21:34:47 +0300

    Create 01. Concatenate Data.js

---
+++ Programming Basics with JavaScript/01. First Steps in Coding - LAB/01. Concatenate Data.js
@@ -0,0 +1,12 @@
+function concatenateData(input){
+    let name = input[0];
+    let lastname = input[1];
+    let age = input[2];
+    let town = input[3];
+
+    console.log(`You are ${name} ${lastname}, a ${age}-years old person from ${town}`);
+
+
+}
+
+concatenateData(['Maria', 'Ivanova', '32', 'Sofia'])
