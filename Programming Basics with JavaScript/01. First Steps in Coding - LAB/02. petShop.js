d4d3e1469922e8bf36c59b6b80a4789015d76d4d
Georgi Raynov <jornalo891@gmail.com>
2022-07-24 22:01:53 +0300

    Add files via upload

---
+++ Programming Basics with JavaScript/01. First Steps in Coding - LAB/02. petShop.js
@@ -0,0 +1,13 @@
+function petShop(input){
+
+    let dogsFood = Number(input[0]);
+    let catsFood = Number(input[1]);
+
+    let spendedMoney = (dogsFood * 2.5) + (catsFood * 4);
+
+    console.log(spendedMoney);
+
+
+
+}
+petShop["5 ", "4 "]
