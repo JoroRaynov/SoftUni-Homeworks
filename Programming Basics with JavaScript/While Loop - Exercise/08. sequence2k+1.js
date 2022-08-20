761da7908fcd8dc1982fadc758b00b31db022f83
Georgi Raynov <jornalo891@gmail.com>
2022-08-20 21:43:39 +0300

    Add files via upload

---
+++ Programming Basics with JavaScript/While Loop - Exercise/08. sequence2k+1.js
@@ -0,0 +1,17 @@
+function sequence2kplus1(input){
+
+    let index = 0;
+
+    let num = Number(input[index]);
+    let sumNum = 1;
+
+    while(sumNum <= num ){
+        console.log(sumNum);
+        sumNum = sumNum * 2 + 1
+        
+        }
+        
+
+    }
+    
+sequence2kplus1(['31'])
